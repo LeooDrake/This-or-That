31953dfd7f68d0611e9abc1572642838dea14a53
jlabruna <justin.labruna@gmail.com>
2023-06-22 22:52:04 +1000

    lot of changes too many to list

--- src/client/components/onload/header.js
+++ src/client/components/onload/header.js
@@ -2,7 +2,8 @@ export default function renderHeader(){
   let header = document.getElementById("header");
   header.innerHTML = 
     `
-      <h1>This or That</h1>
+    <a class="link-light link-underline-opacity-25 link-underline-opacity-100-hover" href="/"><h1>This or That</h1></a>
+
     `
     ;
 }
