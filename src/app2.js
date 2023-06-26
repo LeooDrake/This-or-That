ebe76dc001fcd5921bbe4efb14b2c905d7a2004a
axwong <alexwong@tadyen.dev>
2023-06-26 19:35:21 +1000

    mild cleanup of pointless shit

--- src/app2.js
+++ src/app2.js
@@ -1,24 +1,19 @@
 import express from "express";
-import {errorHandler} from "./utils/errorHandler.js"
-import { appDb } from "./db/appDb.js";
 
 // routers
 import {router as apiRouter} from "./api/indexRoute.js";
 const clientRouter = express.Router();
 clientRouter.use(express.static("./src/client"));
 
+// app
 const app = express();
 const port = process.env.PORT || 3001;
 
+// mount routes
 app.use("/", clientRouter);
 app.use("/api", apiRouter);
 
-try{
-    app.listen(port, ()=>{
-        console.log(`listening on port ${port}`);
-    })
-}catch(e){
-    console.log("AHFGDSHJFGHHJDSGHJFGJHSD");
-    appDb.db.close();
-    errorHandler(e, true);
-}
+// start listening
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
