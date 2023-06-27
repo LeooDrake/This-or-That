0b6542ecab4bf97ce39fa24cd082f7a2e27e2d44
axwong <alexwong@tadyen.dev>
2023-06-28 00:04:04 +1000

    changed upvote from PATCH to GET cus cant make it work otherwise without great pain

--- src/api/upvoteRoute.js
+++ src/api/upvoteRoute.js
@@ -5,7 +5,7 @@ import {appDb} from "../db/appDb.js";
 import {error500} from "../utils/errorHandler.js";
 /*
     /vote/:id
-        .PATCH({})
+        .GET({})
             Upvotes the submissioned referenced by :id from the db.
     Params:
         :id: mongoose.Types.ObjectId    // _id value of Submissions object as string.
@@ -17,13 +17,13 @@ await appDb.ready;
 const Submissions = appDb.models.Submissions;
 
 router.route('/upvote/:id')
-    .patch(async (req,res)=>{
+    .get(async (req,res)=>{
         try{
             var id = new mongoose.Types.ObjectId(req.params.id);
             let submission = await Submissions.where({_id: id}).findOne().exec();
             submission.total_votes = Number(submission.total_votes) + 1;
             await submission.save();
-            res.status(200).json({'message':`successfully upvoted${req.params.id}`});
+            res.status(200).json({'message':`successfully upvoted ${req.params.id}`});
         }catch(e){error500(e,res)}
     })
 ;
